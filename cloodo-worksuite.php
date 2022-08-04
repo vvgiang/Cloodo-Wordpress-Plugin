8a28ecab580a8bec8407f0fab7266b5e9d4d8513
Binh <giangvv.cntt@gmail.com>
2022-08-04 09:29:11 +0700

    create header

---
+++ cloodo-worksuite.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       Cloodo Worksuite
+ * Plugin URI:        https://worksuite.cloodo.com/
+ * Description:       Project management, trusted badge review
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Cloodo
+ * Author URI:        https://cloodo.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       cloodo-worksuite
+ * Domain Path:       /languages
+ */
+
