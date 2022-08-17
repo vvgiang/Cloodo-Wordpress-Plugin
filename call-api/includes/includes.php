7c2e0894708ec83b72695524943680c562a7c28d
vvgiang <33439115+vvgiang@users.noreply.github.com>
2022-08-17 11:15:33 +0700

    include jquery

--- call-api/includes/includes.php
+++ call-api/includes/includes.php
@@ -4,7 +4,8 @@ function cw_add_style(){
         wp_enqueue_style( 'cw-style',plugin_dir_url(__DIR__).'admin/css/style.css');
         wp_enqueue_style( 'cw-boostrap',plugin_dir_url(__DIR__).'admin/css/bootstrap.css');
         wp_enqueue_style( 'cw-awesome',plugin_dir_url(__DIR__).'admin/fontawesome/css/all.min.css');
-        wp_enqueue_script( 'cw-jquery',plugin_dir_url(__DIR__).'admin/js/bootstrapjs.min.js');
+        wp_enqueue_script( 'cw-modal',plugin_dir_url(__DIR__).'admin/js/bootstrapjs.min.js');
+        wp_enqueue_script( 'cw-jquery',plugin_dir_url(__DIR__).'admin/js/jquery.min.js');
         wp_enqueue_script( 'cw-script',plugin_dir_url(__DIR__).'admin/js/script.js');
     }
     add_action('admin_enqueue_scripts', 'cw_addstyle');
