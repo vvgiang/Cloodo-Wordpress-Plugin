624de769be9b20a89388c9736d3206c30db485d1
vvgiang <33439115+vvgiang@users.noreply.github.com>
2022-09-14 16:40:51 +0700

    v2

--- includes/includes-project.php
+++ includes/includes-project.php
@@ -1,17 +1,21 @@
 <?php
 function clws_add_style(){
     function clws_addstyle(){
-        wp_register_style( 'cw-boostrap',plugins_url('admin/css/bootstrap.css',__DIR__));
-        wp_enqueue_style('cw-boostrap');
-        wp_register_style( 'cw-style',plugins_url('admin/css/style-project.css',__DIR__));
-        wp_enqueue_style('cw-style');
-        wp_register_style( 'cw-awesome',plugins_url('admin/fontawesome/css/all.min.css',__DIR__));
-        wp_enqueue_style('cw-awesome');
-        wp_register_script( 'cw-modal',plugins_url('admin/js/bootstrapjs.min.js',__DIR__));
-        wp_enqueue_script('cw-modal');
+        wp_register_style( 'clws-boostrap',plugins_url('admin/css/bootstrap.css',__DIR__));
+        wp_enqueue_style('clws-boostrap');
+        wp_register_style( 'clws-style',plugins_url('admin/css/style-project.css',__DIR__));
+        wp_enqueue_style('clws-style');
+        wp_register_style( 'clws-awesome-font',plugins_url('admin/css/fontawesome.min.css',__DIR__));
+        wp_enqueue_style('clws-awesome-font');
+        wp_register_style( 'clws-awesome-brands',plugins_url('admin/css/brands.min.css',__DIR__));
+        wp_enqueue_style('clws-awesome-brands');
+        wp_register_style( 'clws-awesome-solid',plugins_url('admin/css/solid.min.css',__DIR__));
+        wp_enqueue_style('clws-awesome-solid');
+        wp_register_script( 'clws-modal',plugins_url('admin/js/bootstrapjs.min.js',__DIR__));
+        wp_enqueue_script('clws-modal');
         wp_enqueue_script('jQuery');
-        wp_register_script( 'cw-script',plugins_url('admin/js/script-project.js',__DIR__));
-        wp_enqueue_script('cw-script');
+        wp_register_script( 'clws-script',plugins_url('admin/js/script-project.js',__DIR__));
+        wp_enqueue_script('clws-script');
     }
     add_action('admin_enqueue_scripts', 'clws_addstyle');
 }
